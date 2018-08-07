057a170ac358935a29699c76f92de542c40efa90
CSC573LAB <42153361+CSC573LAB@users.noreply.github.com>
2018-08-07 00:46:23 -0400

    removed some extra lines from background.sh file

--- background.sh
+++ background.sh
@@ -9,9 +9,3 @@ sudo iptables -t mangle -F
 sudo iptables -F
 sudo iptables -X
 sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
-sudo iptables -t nat -A PREROUTING -d 152.46.18.224 -p tcp --dport 1702 -j DNAT --to 172.17.0.2:22
-sudo iptables -t nat -A PREROUTING -d 152.46.18.224 -p tcp --dport 1703 -j DNAT --to 172.17.0.3:22
-sudo iptables -t nat -A PREROUTING -d 152.46.18.224 -p tcp --dport 1704 -j DNAT --to 172.17.0.4:22
-sudo iptables -t nat -A PREROUTING -d 152.46.18.224 -p tcp --dport 1705 -j DNAT --to 172.17.0.5:22
-sleep 3
-done
