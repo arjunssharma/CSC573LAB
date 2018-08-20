11e6efa7adb8e5d062a37c87ca3bc995450f82b0
CSC573LAB <42153361+CSC573LAB@users.noreply.github.com>
2018-08-20 18:25:13 -0400

    Update csc-lab.py

--- csc-lab.py
+++ csc-lab.py
@@ -2,7 +2,6 @@ from math import *
 import os
 import subprocess
 import sys
-import pexpect
 
 node_matrix = open('matrix.txt','r')     #  To open a file in read mode
 node = node_matrix.read()
@@ -10,12 +9,12 @@ node_row = node.strip().split("\n")
 nodes = node_row[0].strip().split()
 node_num = len(nodes)
 
-pids=['pids']
+PID=['p']
 for i in range(0,node_num):
 # To get the process ID of hosts(containers)
   output = subprocess.Popen("sudo docker inspect -f '{{.State.Pid}}' "+nodes[i], stdout=subprocess.PIPE, shell=True)
   (out, err) = output.communicate()
-  pids.append(out.strip())
+  PID.append(out.strip())
 
 # i = 0
 # j = 0
@@ -34,8 +33,8 @@ for i in range(1,(node_num+1)):
 
         if current_node[j] == "1":
             os.system("sudo ip link add veth1 type veth peer name veth2")  #To create veth pairs to connect the containers as per topology
-            os.system("sudo ip link set dev veth1 netns " + pids[i] + " name eth" + str(j) + " up")
-            os.system("sudo ip link set dev veth2 netns " + pids[j] + " name eth" + str(i) + " up")
+            os.system("sudo ip link set dev veth1 netns " + PID[i] + " name eth" + str(j) + " up")
+            os.system("sudo ip link set dev veth2 netns " + PID[j] + " name eth" + str(i) + " up")
 for i in nodes:
     os.system("sudo docker exec "+i+" /etc/init.d/ssh restart")
     child = pexpect.spawn("sudo docker exec -i -t " + i + " passwd root")
