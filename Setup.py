0d3cf728606e456b00541976560eed02ce1a33b1
CSC573LAB <42153361+CSC573LAB@users.noreply.github.com>
2018-10-18 21:00:11 -0400

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -7,7 +7,7 @@ HOST_IP=fip.read().strip()
 bg_file = open('background.sh', 'w')
 bg_file.close()
 bg_file = open('background.sh','a')  #opening file in append mode
-bg_file.write("#!/bin/bash"+"\n"+"#To Allow wireshark to access internal ports of hosts by adding iptable rules"+"\n"+"while true; do"+"\n"+"sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE"+"\n")
+bg_file.write("#!/bin/bash"+"\n"+"#To Allow wireshark to access internal ports of hosts by adding iptable rules"+"\n"+"while true; do"+"\n"+"sudo iptables -P INPUT ACCEPT"+"\n"+"sudo iptables -P FORWARD ACCEPT"+"\n"+"sudo iptables -P OUTPUT ACCEPT"+"\n"+"sudo iptables -t nat -F"+"\n"+"sudo iptables -t mangle -F"+"\n"+"sudo iptables -F"+"\n"+"sudo iptables -X"+"\n"+"sudo iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE"+"\n")
 #bg_file.write("\n")
 for i in topology_line:
 #To get docker IP using this command
